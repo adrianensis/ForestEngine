82962859a3a403b405fb33abd756ec8b292c65a8
adrianensis <adrianensis@gmail.com>
2023-08-06 15:49:27 +0200

    build script refactor - isolate profiler gui building

--- scripts/build.py
+++ scripts/build.py
@@ -12,6 +12,8 @@ buildDir="build"
 dependenciesDir="dependencies"
 binariesDir="binaries"
 
+profilerDir = os.path.join(dependenciesDir, "easy_profiler-2.1.0")
+
 buildType="Debug"
 
 buildUnitTests=False
@@ -52,11 +54,6 @@ for opt, arg in opts:
     elif opt in ['-p']:
       enableProfiler=True
 
-if not os.path.isdir(buildDir):
-      os.mkdir(buildDir)
-
-os.chdir(buildDir)
-
 system_name = platform.system()
 system_info = str(platform.uname())
 
@@ -64,15 +61,19 @@ print(system_name)
 print(system_info)
 
 cmake_generator = ""
+systemBuildCommand=""
 if system_name == "Linux" or system_name == "Linux2":
     # linux
     cmake_generator = '-G "Unix Makefiles"'
+    systemBuildCommand = 'make -j8'
 elif system_name == "Darwin":
     # OS X
     pass
 elif system_name == "Windows":
     # Windows...
     cmake_generator = '-G "Visual Studio 17 2022"'
+    # Requires msbuild added to the PATH
+    systemBuildCommand = 'msbuild forest.sln'
 # elif ANDROID:
 #     # Windows...
 #     pass
@@ -80,54 +81,72 @@ elif system_name == "Windows":
 #     # Windows...
 #     pass
 
+if not os.path.isdir(buildDir):
+      os.mkdir(buildDir)
+
+##########################################
+########## PRE BUILD ###########
+##########################################
+
+# easy_profiler GUI
+if enableProfiler:
+    os.chdir(profilerDir)
+
+    if not os.path.isdir(buildDir):
+      os.mkdir(buildDir)
+
+    os.chdir(buildDir)
+
+    buildProfilerCommandArgs = [
+        "-DCMAKE_BUILD_TYPE=Release",
+    ]
+
+    buildProfilerCommandArgsString =" ".join(buildProfilerCommandArgs)
+
+    buildProfilerCommand = 'cmake {cmake_generator} {buildProfilerCommandArgsString} ..'.format(
+    cmake_generator = cmake_generator,
+    buildProfilerCommandArgsString = buildProfilerCommandArgsString)
+    print(buildProfilerCommand)
+    os.system(buildProfilerCommand)
+    os.system(systemBuildCommand)
+
+##########################################
+########## BUILD ###########
+##########################################
+os.chdir(os.path.join(cwd, buildDir))
+
 # -DCMAKE_C_COMPILER=/usr/bin/clang -DCMAKE_CXX_COMPILER=/usr/bin/clang++
 
 buildCommandArgs = [
-"-DCMAKE_BUILD_TYPE=" + str(buildType),
-"-DBUILD_UNIT_TESTS=" + str(buildUnitTests),
-"-DBUILD_INTEGRATION_TESTS=" + str(buildIntegrationTests),
-"-DTOOLS_TO_BUILD=" + "\"" + str(";".join(toolsToBuild) + "\""),
-"-DAPPS_TO_BUILD=" + "\"" + str(";".join(appsToBuild) + "\""),
-"-DENABLE_LOGS=" + str(enableLogs),
-"-DENABLE_PROFILER=" + str(enableProfiler)
+    "-DCMAKE_BUILD_TYPE=" + str(buildType),
+    "-DBUILD_UNIT_TESTS=" + str(buildUnitTests),
+    "-DBUILD_INTEGRATION_TESTS=" + str(buildIntegrationTests),
+    "-DTOOLS_TO_BUILD=" + "\"" + str(";".join(toolsToBuild) + "\""),
+    "-DAPPS_TO_BUILD=" + "\"" + str(";".join(appsToBuild) + "\""),
+    "-DENABLE_LOGS=" + str(enableLogs),
+    "-DENABLE_PROFILER=" + str(enableProfiler)
 ]
 
 buildCommandArgsString =" ".join(buildCommandArgs)
 
 buildCommand = 'cmake {cmake_generator} {buildCommandArgsString} ..'.format(
   cmake_generator = cmake_generator,
-  buildCommandArgsString = buildCommandArgsString,
-)
+  buildCommandArgsString = buildCommandArgsString)
 print(buildCommand)
 os.system(buildCommand)
 
-if system_name == "Linux" or system_name == "Linux2":
-    # linux
-    os.system('make -j8')
-elif system_name == "Darwin":
-    # OS X
-    pass
-elif system_name == "Windows":
-    # Windows...
-    # Requires msbuild added to the PATH
-    os.system('msbuild forest.sln')
-# elif ANDROID:
-#     # Windows...
-#     pass
-# elif IOS:
-#     # Windows...
-#     pass
+os.system(systemBuildCommand)
 
 # go back to root folder after build
 os.chdir(cwd)
 
-#####################
-# POST BUILD
-#####################
+##########################################
+########## POST BUILD ###########
+##########################################
 
 # easy_profiler: create bin link
 if enableProfiler:
-    easy_profiler_bin_path_source = os.path.join(cwd, os.path.join(os.path.join(buildDir, os.path.join(dependenciesDir, "easy_profiler-2.1.0/bin")), "profiler_gui"))
+    easy_profiler_bin_path_source = os.path.join(cwd, os.path.join(dependenciesDir, "easy_profiler-2.1.0/bin"), "profiler_gui")
     bin_gui_path_destiny = os.path.join(cwd, os.path.join(os.path.join(binariesDir, buildType), "profiler_gui"))
     try:
         os.remove(bin_gui_path_destiny)
