ba800545b6d6977a75c0bd72cf216910c4386c4e
adrianensis <adrianensis@gmail.com>
2023-09-17 17:00:22 +0200

    fix texture config

--- code/Graphics/GPU/GPUInterface.cpp
+++ code/Graphics/GPU/GPUInterface.cpp
@@ -211,8 +211,7 @@ GLuint GPUInterface::createTexture(u32 width, u32 height, const byte* data, bool
     if(createMipMap)
     {
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-        glGenerateMipmap(GL_TEXTURE_2D);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
     }
     else
     {
@@ -220,8 +219,13 @@ GLuint GPUInterface::createTexture(u32 width, u32 height, const byte* data, bool
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
     }
     
-    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, data);
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, data);
     
+    if(createMipMap)
+    {
+        glGenerateMipmap(GL_TEXTURE_2D);
+    }
+
     disableTexture();
 
     return textureId;
