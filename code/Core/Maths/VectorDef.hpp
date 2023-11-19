7a8eb2e5965d77821a08faef06bfafbd9666163d
adrianensis <adrianensis@gmail.com>
2023-11-19 20:46:34 +0100

    vector macros refactor

--- code/Core/Maths/VectorDef.hpp
+++ code/Core/Maths/VectorDef.hpp
@@ -4,105 +4,66 @@
 #include "Core/Maths/MathUtils.hpp"
 #include "Core/Assert/Assert.hpp"
 
-#define VECTOR2_MEMBERS() \
-    f32 x = 0; \
-    f32 y = 0; 
+#define VECTOR_COMPONENT_1 x
+#define VECTOR_COMPONENT_2 y
+#define VECTOR_COMPONENT_3 z
+#define VECTOR_COMPONENT_4 w
 
-#define VECTOR3_MEMBERS() \
-    VECTOR2_MEMBERS(); \
-    f32 z = 0; 
+#define VECTOR_FOR_EACH_1(macro, ...) \
+    macro(VECTOR_COMPONENT_1 __VA_OPT__(,) __VA_ARGS__)
+#define VECTOR_FOR_EACH_2(macro, ...) \
+    VECTOR_FOR_EACH_1(macro, __VA_ARGS__) \
+    macro(VECTOR_COMPONENT_2 __VA_OPT__(,) __VA_ARGS__)
+#define VECTOR_FOR_EACH_3(macro, ...) \
+    VECTOR_FOR_EACH_2(macro, __VA_ARGS__) \
+    macro(VECTOR_COMPONENT_3 __VA_OPT__(,) __VA_ARGS__)
+#define VECTOR_FOR_EACH_4(macro, ...) \
+    VECTOR_FOR_EACH_3(macro, __VA_ARGS__) \
+    macro(VECTOR_COMPONENT_4 __VA_OPT__(,) __VA_ARGS__)
 
-#define VECTOR4_MEMBERS() \
-    VECTOR3_MEMBERS(); \
-    f32 w = 0; 
+#define VECTOR_FOR_EACH(n, macro, ...) VECTOR_FOR_EACH_##n(macro, __VA_ARGS__)
+
+#define VECTOR_DEFINE_COMPONENTS(comp) f32 comp = 0;
 
 #define VECTOR2_PARAMS() f32 x, f32 y
-#define VECTOR3_PARAMS() VECTOR2_PARAMS(), f32 z
+#define VECTOR3_PARAMS() VECTOR2_PARAMS(), f32 z 
 #define VECTOR4_PARAMS() VECTOR3_PARAMS(), f32 w
+#define VECTOR_PARAMS(n) VECTOR##n##_PARAMS()
 
-#define VECTOR2_OP_VECTOR(op) \
-    this->x = this->x op rhs.x; \
-    this->y = this->y op rhs.y; 
-
-#define VECTOR3_OP_VECTOR(op) \
-    VECTOR2_OP_VECTOR(op); \
-    this->z = this->z op rhs.z; 
-
-#define VECTOR4_OP_VECTOR(op) \
-    VECTOR3_OP_VECTOR(op); \
-    this->w = this->w op rhs.w;
-    
-#define VECTOR2_OP_F32(op) \
-    this->x = this->x op rhs; \
-    this->y = this->y op rhs; 
-
-#define VECTOR3_OP_F32(op) \
-    VECTOR2_OP_F32(op); \
-    this->z = this->z op rhs; 
-
-#define VECTOR4_OP_F32(op) \
-    VECTOR3_OP_F32(op); \
-    this->w = this->w op rhs; 
-
-#define VECTOR2_SET_F32() \
-    this->x = x; \
-    this->y = y; 
-
-#define VECTOR3_SET_F32() \
-    VECTOR2_SET_F32(); \
-    this->z = z; 
-
-#define VECTOR4_SET_F32() \
-    VECTOR3_SET_F32(); \
-    this->w = w; 
-
-#define VECTOR2_APPLY_FUNC(func) \
-    this->x = func(x); \
-    this->y = func(y); 
-
-#define VECTOR3_APPLY_FUNC(func) \
-    VECTOR2_APPLY_FUNC(func); \
-    this->z = func(z); 
-
-#define VECTOR4_APPLY_FUNC(func) \
-    VECTOR3_APPLY_FUNC(func); \
-    this->w = func(w); 
-
-#define VECTOR2_SET_VECTOR() \
-    this->x = rhs.x; \
-    this->y = rhs.y; 
-
-#define VECTOR3_SET_VECTOR() \
-    VECTOR2_SET_VECTOR(); \
-    this->z = rhs.z; 
-
-#define VECTOR4_SET_VECTOR() \
-    VECTOR3_SET_VECTOR(); \
-    this->w = rhs.w; 
+#define VECTOR_OP_VECTOR(comp, op) this->comp = this->comp op rhs.comp;
+#define VECTOR_OP_F32(comp, op) this->comp = this->comp op rhs;
+#define VECTOR_SET_VECTOR(comp) this->comp = rhs.comp;
+#define VECTOR_SET_F32(comp) this->comp = comp;
+#define VECTOR_APPLY_FUNC(comp, func) this->comp = func(comp);
 
 #define VECTOR2_DOT(v) (this->x * v.x) + (this->y * v.y)
 #define VECTOR3_DOT(v) VECTOR2_DOT(v) + (this->z * v.z)
 #define VECTOR4_DOT(v) VECTOR3_DOT(v) + (this->w * v.w)
+#define VECTOR_DOT(n, v) VECTOR##n##_DOT(v)
 
 #define VECTOR2_MIN_MAX(minOrMaxFunc) std:: minOrMaxFunc (x, y)
 #define VECTOR3_MIN_MAX(minOrMaxFunc) std:: minOrMaxFunc (VECTOR2_MIN_MAX(minOrMaxFunc), z)
 #define VECTOR4_MIN_MAX(minOrMaxFunc) std:: minOrMaxFunc (VECTOR3_MIN_MAX(minOrMaxFunc), w)
+#define VECTOR_MAX(n) VECTOR##n##_MIN_MAX(max)
+#define VECTOR_MIN(n) VECTOR##n##_MIN_MAX(min)
 
 #define VECTOR2_EQ_E(v, e) MathUtils::eqf(this->x, v.x, e) and MathUtils::eqf(this->y, v.y, e)
 #define VECTOR3_EQ_E(v, e) VECTOR2_EQ_E(v, e) and MathUtils::eqf(this->z, v.z, e)
 #define VECTOR4_EQ_E(v, e) VECTOR3_EQ_E(v, e) and MathUtils::eqf(this->w, v.w, e)
+#define VECTOR_EQ_E(n, v, e) VECTOR##n##_EQ_E(v, e)
 
 #define VECTOR2_EQ(v) MathUtils::eqf(this->x, v.x) and MathUtils::eqf(this->y, v.y)
 #define VECTOR3_EQ(v) VECTOR2_EQ(v) and MathUtils::eqf(this->z, v.z)
 #define VECTOR4_EQ(v) VECTOR3_EQ(v) and MathUtils::eqf(this->w, v.w)
+#define VECTOR_EQ(n, v) VECTOR##n##_EQ(v)
 
-#define VECTOR_BASE_DEFINITION(vectorLength) \
-GENERATE_METADATA_STRUCT(Vector##vectorLength)  \
+#define VECTOR_BASE_DEFINITION(n) \
+GENERATE_METADATA_STRUCT(Vector##n)  \
 \
 private:\
     void checkBoundaries(u32 index) const\
     {\
-        CHECK_MSG(index >= 0 and index < vectorLength, "Index out of bounds.");\
+        CHECK_MSG(index >= 0 and index < n, "Index out of bounds.");\
     }\
     f32& get(u32 index)\
     {\
@@ -115,32 +76,32 @@ private:\
         return *(&x + index);\
     }\
 public:\
-    VECTOR##vectorLength##_MEMBERS(); \
+    VECTOR_FOR_EACH(n, VECTOR_DEFINE_COMPONENTS); \
     \
-    Vector##vectorLength() {}\
-    Vector##vectorLength(VECTOR##vectorLength##_PARAMS()) {VECTOR##vectorLength##_SET_F32();}\
-    Vector##vectorLength(const ThisClass& rhs) {VECTOR##vectorLength##_SET_VECTOR();}\
-    ThisClass& set(VECTOR##vectorLength##_PARAMS()) {VECTOR##vectorLength##_SET_F32(); return *this; }\
-    ThisClass& set(const ThisClass& rhs) {VECTOR##vectorLength##_SET_VECTOR(); return *this; }\
-    ThisClass& add(const ThisClass& rhs) { VECTOR##vectorLength##_OP_VECTOR(+); return *this; }\
-    ThisClass& sub(const ThisClass& rhs) { VECTOR##vectorLength##_OP_VECTOR(-); return *this; }\
-    ThisClass& mul(const ThisClass& rhs) { VECTOR##vectorLength##_OP_VECTOR(*); return *this; }\
-    ThisClass& div(const ThisClass& rhs) { VECTOR##vectorLength##_OP_VECTOR(/); return *this; }\
-    ThisClass& add(f32 rhs) { VECTOR##vectorLength##_OP_F32(+); return *this; }\
-    ThisClass& sub(f32 rhs) { VECTOR##vectorLength##_OP_F32(-); return *this; }\
-    ThisClass& mul(f32 rhs) { VECTOR##vectorLength##_OP_F32(*); return *this; }\
-    ThisClass& div(f32 rhs) { VECTOR##vectorLength##_OP_F32(/); return *this; }\
-    ThisClass& abs() { VECTOR##vectorLength##_APPLY_FUNC(std::abs); return *this; }\
-    f32 dot(const ThisClass& v) const { return VECTOR##vectorLength##_DOT(v); } \
+    Vector##n() {}\
+    Vector##n(VECTOR_PARAMS(n)) {VECTOR_FOR_EACH(n, VECTOR_SET_F32); }\
+    Vector##n(const ThisClass& rhs) {VECTOR_FOR_EACH(n, VECTOR_SET_VECTOR);}\
+    ThisClass& set(VECTOR_PARAMS(n)) {VECTOR_FOR_EACH(n, VECTOR_SET_F32); return *this; }\
+    ThisClass& set(const ThisClass& rhs) {VECTOR_FOR_EACH(n, VECTOR_SET_VECTOR); return *this; }\
+    ThisClass& add(const ThisClass& rhs) { VECTOR_FOR_EACH(n, VECTOR_OP_VECTOR, +); return *this; }\
+    ThisClass& sub(const ThisClass& rhs) { VECTOR_FOR_EACH(n, VECTOR_OP_VECTOR, -); return *this; }\
+    ThisClass& mul(const ThisClass& rhs) { VECTOR_FOR_EACH(n, VECTOR_OP_VECTOR, *); return *this; }\
+    ThisClass& div(const ThisClass& rhs) { VECTOR_FOR_EACH(n, VECTOR_OP_VECTOR, /); return *this; }\
+    ThisClass& add(f32 rhs) { VECTOR_FOR_EACH(n, VECTOR_OP_F32, +); return *this; }\
+    ThisClass& sub(f32 rhs) { VECTOR_FOR_EACH(n, VECTOR_OP_F32, -); return *this; }\
+    ThisClass& mul(f32 rhs) { VECTOR_FOR_EACH(n, VECTOR_OP_F32, *); return *this; }\
+    ThisClass& div(f32 rhs) { VECTOR_FOR_EACH(n, VECTOR_OP_F32, /); return *this; }\
+    ThisClass& abs() { VECTOR_FOR_EACH(n, VECTOR_APPLY_FUNC, std::abs); return *this; }\
+    f32 dot(const ThisClass& v) const { return VECTOR_DOT(n, v); } \
     f32 sqrlen() const { return this->dot(*this); } \
     f32 sqrdst(const ThisClass& v) const { ThisClass sub = ThisClass(v) - (*this); return sub.dot(sub); } \
     f32 len() const { return sqrtf(this->sqrlen()); } \
-    f32 max() const { return VECTOR##vectorLength##_MIN_MAX(max); } \
-    f32 min() const { return VECTOR##vectorLength##_MIN_MAX(min); } \
+    f32 max() const { return VECTOR_MAX(n); } \
+    f32 min() const { return VECTOR_MIN(n); } \
     ThisClass& nor() { this->div(this->len()); return *this; } \
     f32 dst(const ThisClass& v) const { return sqrtf(this->sqrdst(v)); } \
-    bool eq(const ThisClass& v, f32 e) const { return VECTOR##vectorLength##_EQ_E(v,e); } \
-    bool eq(const ThisClass& v) const { return VECTOR##vectorLength##_EQ(v); } \
+    bool eq(const ThisClass& v, f32 e) const { return VECTOR_EQ_E(n,v,e); } \
+    bool eq(const ThisClass& v) const { return VECTOR_EQ(n,v); } \
     ThisClass& lerp(const ThisClass& target, f32 t) { (*this) += ((ThisClass(target) - (*this)) * t); return *this; } \
     ThisClass& clamp(f32 maxLength)\
     {\
