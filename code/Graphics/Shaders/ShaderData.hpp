0f36547b0bd28e00d34ef427bee2c52ed263ed69
adrianensis <adrianensis@gmail.com>
2023-11-25 19:08:45 +0100

    remove unused header

--- code/Graphics/Shaders/ShaderData.hpp
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-#include "Core/Module.hpp"
-
-// TODO: this data is supposed to be fed to the ShaderBuilder, somehow
-// it will require a big refactor of GPUMeshBuffer and MeshBatcher, probably Batch as well...
