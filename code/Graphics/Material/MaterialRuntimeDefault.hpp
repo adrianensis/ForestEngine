94b1799b4faf363eb8b70f2a2b0f745c30fe3266
adrianensis <adrianensis@gmail.com>
2024-04-14 01:14:05 +0200

    remove specular material runtime

--- code/Graphics/Material/MaterialRuntimeDefault.hpp
+++ code/Graphics/Material/MaterialRuntimeDefault.hpp
@@ -35,39 +35,6 @@ protected:
     void registerFunctionCalculateBoneTransform(ShaderBuilder& shaderBuilder) const;
 };
 
-// PBR SPECULAR
-
-class MaterialPropertiesBlockPBRSpecularGlossiness
-{
-public:
-    Vector3 mDiffuse = Vector3(1,1,1);
-    alignas(16) Vector3 mSpecular = Vector3(0.2f,0.2f,0.2f);
-    alignas(16) Vector3 mGlossiness = Vector3(32,0,0);
-};
-
-class MaterialRuntimePBRSpecularGlossiness : public MaterialRuntimeDefault
-{
-public:
-    void createFragmentShader(ShaderBuilder& shaderBuilder, const GPUVertexBuffersContainer& gpuVertexBuffersContainer, const GPUSharedBuffersContainer& gpuSharedBuffersContainer) const;
-
-protected:
-    virtual std::vector<GPUStructDefinition::GPUStructVariable> generateMaterialPropertiesBlock() override;
-
-    virtual void fragmentShaderBaseColor(ShaderBuilder& shaderBuilder) const override;
-    virtual void fragmentShaderShadingModel(ShaderBuilder& shaderBuilder) const override;
-
-    virtual void registerFragmentShaderData(ShaderBuilder& shaderBuilder, const GPUVertexBuffersContainer& gpuVertexBuffersContainer, const GPUSharedBuffersContainer& gpuSharedBuffersContainer) const override;
-
-    void registerFunctionCalculatePBRSpecularGlossiness(ShaderBuilder& shaderBuilder) const;
-
-protected:
-    inline static const GPUFunctionDefinition mCalculatePBRSpecularGlossiness
-    {
-        GPUBuiltIn::PrimitiveTypes::mVector4,
-        "calculatePBRSpecularGlossiness"
-    };
-};
-
 // PBR METALLIC
 
 class MaterialPropertiesBlockPBRMetallicRoughness
