3500e0fd824e69d309ee91cb93873797361282b5
adrianensis <adrianensis@gmail.com>
2024-08-22 11:10:50 +0200

    vulkan 1.3

--- code/Graphics/GPU/Vulkan.cpp
+++ code/Graphics/GPU/Vulkan.cpp
@@ -88,7 +88,7 @@ bool Vulkan::createInstance() {
     appInfo.applicationVersion = VK_MAKE_VERSION(config.MajorVersion, config.MinorVersion, config.PatchVersion);
     appInfo.pEngineName = config.Name.c_str();
     appInfo.engineVersion = VK_MAKE_VERSION(config.MajorVersion, config.MinorVersion, config.PatchVersion);
-    appInfo.apiVersion = VK_API_VERSION_1_2;
+    appInfo.apiVersion = VK_API_VERSION_1_3;
 
     VkInstanceCreateInfo createInfo{};
     createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
