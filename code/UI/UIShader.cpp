fe750d8a6f5f9b73608c778d9a01ede408a24a42
adrianensis <adrianensis@gmail.com>
2025-01-11 22:31:26 +0100

    fix ui shader

--- code/UI/UIShader.cpp
+++ code/UI/UIShader.cpp
@@ -35,17 +35,20 @@ void ShaderUI::fragmentShaderCode(ShaderBuilder& shaderBuilder) const
     // NEXT: Restore ui/font texture
     auto& inTextureCoord = shaderBuilder.get().getAttribute(GPUShaderDefinitions::FragmentInput::mTextureCoords.at(0));
     auto& textureHandler = shaderBuilder.get().getAttribute(GPUShaderDefinitions::Uniforms::getTextureHandler(TextureBindingNames::smBaseColor));
-    if(inTextureCoord.isValid())
+    if(textureHandler.isValid())
     {
-        // auto& texturesBuffer = shaderBuilder.get().getUniformBuffer(GPUShaderDefinitions::UniformBuffers::mTextures.mInstanceName);    
-        // Variable textures(texturesBuffer.mGPUUniformBufferData.getScopedGPUVariableData(0));
-        shaderBuilder.getMain().
-        // ifBlock(textureHandler.notEq("0"s)).
-            set(outColor, call("texture", {/*textures.at(textureHandler)*/textureHandler, inTextureCoord})).
-            ifBlock(outColor.dot("r").add(outColor.dot("g").add(outColor.dot("b"))).eq({"0"})).
-                line("discard").
-            end();
-        // end();
+        if(inTextureCoord.isValid())
+        {
+            // auto& texturesBuffer = shaderBuilder.get().getUniformBuffer(GPUShaderDefinitions::UniformBuffers::mTextures.mInstanceName);    
+            // Variable textures(texturesBuffer.mGPUUniformBufferData.getScopedGPUVariableData(0));
+            shaderBuilder.getMain().
+            // ifBlock(textureHandler.notEq("0"s)).
+                set(outColor, call("texture", {/*textures.at(textureHandler)*/textureHandler, inTextureCoord})).
+                ifBlock(outColor.dot("r").add(outColor.dot("g").add(outColor.dot("b"))).eq({"0"})).
+                    line("discard").
+                end();
+            // end();
+        }
     }
 }
 
