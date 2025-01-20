d628d58fd2ae0dc70e6668c098bbea55428d8912
adrianensis <adrianensis@gmail.com>
2025-01-20 22:46:53 +0100

    restore ui button text position

--- code/UI/UIElements/UIButton.cpp
+++ code/UI/UIElements/UIButton.cpp
@@ -11,9 +11,6 @@ void UIButton::init()
 {
 	UIPanel::init();
 
-    mClipChildren = true;
-
-
 	subscribeToMouseEvents();
 }
 
@@ -49,7 +46,7 @@ void UIButton::setText(HashedString text)
 			UIBuilder uiBuilder;
 
 			mText = uiBuilder.
-			// setPosition(Vector2(-mConfig.mDisplaySize.x/2.0f, mConfig.mDisplaySize.y/2.0f)).
+			setPosition(Vector2(-mConfig.mDisplaySize.x/2.0f, mConfig.mDisplaySize.y/2.0f)).
 			setSize(mConfig.mDisplaySize).
 			setText(text).
 			setTextScale(mConfig.mTextScale).
