72a6c5ef5e9a702da32432d0bbf07e8b54a36579
adrianensis <adrianensis@gmail.com>
2025-04-18 12:55:21 +0200

    remove unnecessary include

--- code/GPU/ShapeRenderer/GPUShapeRenderer.cpp
+++ code/GPU/ShapeRenderer/GPUShapeRenderer.cpp
@@ -1,7 +1,6 @@
 #include "GPU/ShapeRenderer/GPUShapeRenderer.hpp"
 #include "GPU/Shader/GPUShaderDefinitions.hpp"
 #include "GPU/GPUInstance.hpp"
-#include "Graphics/Camera/Camera.hpp"
 #include "GPU/Shader/ShaderBuilder/GPUShaderBuilder.hpp"
 #include "GPU/Shader/GPUShaderManager.hpp"
 #include "GPU/Shader/BuiltIn/GPUShaderDefault.hpp"
