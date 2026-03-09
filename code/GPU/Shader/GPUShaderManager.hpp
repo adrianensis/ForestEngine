84d75d3b682b7793ddebef1d8c60aec0572b5fac
adrianensis <adrianensis@gmail.com>
2026-03-10 00:14:08 +0100

    remove unused member

--- code/GPU/Shader/GPUShaderManager.hpp
+++ code/GPU/Shader/GPUShaderManager.hpp
@@ -51,7 +51,6 @@ private:
 
 	std::unordered_map<Core::ClassId, GPUShaderPropertyBlockRenderState> mGPUShaderPropertyBlockRenderStates;
 	
-    std::unordered_map<Core::HashedString, Core::WeakPtr<GPUTexture>> mTexturesByPath;
 	std::unordered_map<Core::u32, std::unordered_map<Core::HashedString, Core::WeakPtr<GPUTexture>>> mTextureBindingsByShader;
     std::vector<Core::OwnerPtr<GPUShader>> mShaders;
     std::vector<Core::OwnerPtr<GPUShaderPropertiesInstance>> mGPUShaderPropertiesInstances;
